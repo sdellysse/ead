60a2bfe37d34f4f69585649d079f8b02c48a4a99
Shawn Dellysse <sdellysse@gmail.com>
2018-01-15 03:52:06 -0500

    work

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
 "use strict";
 
-const m = (def) => {
-  const mModule = {
-    exports: {},
-  };
+const merge = (...args) => Object.assign({}, ...args);
 
-  def(mModule);
+const ead = {
+  SYM_IMPL: Symbol("ead.SYM_IMPL"),
+  SYM_DEF:  Symbol("ead.SYM_DEF"),
 
-  return mModule.exports;
+  execute: (tasks, request) => {
+    throw new Error("TODO");
+  },
+
+  createTask: (name, def) => {
+    if (typeof(def) === "function") {
+      return ead.createTask(name, { impl: def });
+    }
+    if (typeof(def.interfaceName) !== "string") {
+      return ead.createTask(name, merge(def, {
+        interfaceName: `taskInterface_${ name }`,
+      }));
+    }
+    if (typeof(def.argsLength) !== "number") {
+      return ead.createTask(name, merge(def, {
+        argsLength: def.impl.length,
+      }));
+    }
+
+    if (!/^[$A-Za-z_][0-9A-Za-z_$]*$/.test(name)) {
+      throw new InvalidNameError(name);
+    }
+
+    const retval = (...args) => ({ request: name, args });
+
+    Object.defineProperties(retval, {
+      name:           { value: def.interfaceName },
+      length:         { value: def.argsLength    },
+      [ead.SYM_IMPL]: { value: def.impl          },
+      [ead.SYM_DEF]:  { value: def               },
+    });
+
+    return retval;
+  },
+
+  createTasks: (defs) => Object.keys(defs).reduce(
+    (acc, name) => merge(acc, {
+      [name]: ead.createTask(name, defs[name]),
+    }),
+
+    {}
+  ),
+
+  replaceTaskImpls: (tasks, defs) => Object.keys(defs).reduce(
+    (acc, name) => {
+      if (typeof(tasks[name]) === "undefined") {
+        throw new InvalidTaskNameError(name);
+      }
+
+      const implCreator = defs[name];
+
+      const previousTaskDef = tasks[name][ead.SYM_DEF];
+      const newTaskDef      = merge(previousTaskDef, {
+        impl: implCreator(previousTaskDef.impl),
+      });
+
+      return merge(acc, {
+        [name]: ead.createTask(name, newTaskDef),
+      });
+    },
+
+    {}
+  ),
 };
 
-const task = m((module) => {
-  const ead = require("ead");
-
-  module.exports = ead.tasks.createAll({
-    http: {
-      get: (url) => fetch(url)
-        .then(res  => res.json())
-        .then(json => ({ response: json }))
-      ,
-    },
-  });
+const tasks = ead.createTasks({
+  httpGet: (url) => fetch(url)
+    .then(res  => res.json())
+    .then(json => ({ response: json }))
+  ,
 });
